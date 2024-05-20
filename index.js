05fcb7690434ca98ec9d0a7f5d976c1a4abb1fbf
Kinga <bhukinga@gmail.com>
2024-05-20 17:42:55 -0400

    completed lab work

---
+++ index.js
@@ -0,0 +1,24 @@
+const express = require('express');
+const path = require('path');
+const imageToAscii = require('image-to-ascii');
+
+const app = express();
+const port = 3000;
+
+app.use(express.static('public'));
+
+app.get('/data', (req, res) => {
+    res.json({ message: "Hello, this is JSON data!" });
+});
+
+app.get('/ascii-image', (req, res) => {
+    const imagePath = path.join(__dirname, 'public/img/flower.jpg');
+    imageToAscii(imagePath, (err, converted) => {
+        if (err) return res.send("Failed to convert image.");
+        res.send("<pre>" + converted + "</pre>");
+    });
+});
+
+app.listen(port, () => {
+    console.log(`Server running at http://localhost:${port}/`);
+});
